0f4525db0cded36a33b8dadd38260f3558c9dafc
BrianWalczak <72092838+BrianWalczak@users.noreply.github.com>
2022-02-09 19:05:41 -0600

    Update import.js

--- import/import.js
+++ import/import.js
@@ -1,6 +1,6 @@
 if("serviceWorker" in navigator){
-			navigator.serviceWorker.register("service-worker.js").then(function(registration){
-				}).catch(function(error){
-					alert("Service worker failed to be registered with error: ",error);
-					});
-					}
+	navigator.serviceWorker.register("service-worker.js").then(function(registration){
+		}).catch(function(error){
+			console.log("Service worker failed to be registered with error: ",error);
+			});
+			}
