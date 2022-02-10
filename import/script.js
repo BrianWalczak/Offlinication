9d36565d07eb9c4320d9b7f9322fbd24e7bd7573
BrianWalczak <72092838+BrianWalczak@users.noreply.github.com>
2022-02-09 18:54:54 -0600

    Update script.js

--- import/script.js
+++ import/script.js
@@ -1,20 +1,6 @@
-self.addEventListener("install",function(event){
-   event.waitUntil(
-      caches.open("our-cache").then(function(cache){
-         cache.addAll(["/"]);
-      })
-   );
-});
-
-self.addEventListener("fetch",function(event){
-	event.respondWith(
-   fetch(event.request).then(function(response){
-      return response;
-   }).catch(function(error){
-		 console.log(error)
-      return caches.match(event.request).then(function (cacheRes) {
-       return cacheRes;
-      })
-   })
-);
-});
+		if("serviceWorker" in navigator){
+			navigator.serviceWorker.register("service-worker.js").then(function(registration){
+				}).catch(function(error){
+					console.log("Service worker failed to be registered with error: ",error);
+					});
+					}
