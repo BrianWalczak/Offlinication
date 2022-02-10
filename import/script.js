d3fff60d9b627048d2d4d9d159f93bd7c9accd67
BrianWalczak <72092838+BrianWalczak@users.noreply.github.com>
2022-02-09 18:56:33 -0600

    Delete script.js

--- import/script.js
+++
@@ -1,6 +0,0 @@
-		if("serviceWorker" in navigator){
-			navigator.serviceWorker.register("service-worker.js").then(function(registration){
-				}).catch(function(error){
-					console.log("Service worker failed to be registered with error: ",error);
-					});
-					}
